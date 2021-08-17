61dcdf9203555e40550b16dfd00364789e29ac26
ubelhj <ubelhj@uw.edu>
2021-08-17 14:07:51 -0700

    Capitalizes stats for selection menus

--- OBSCounter/version.h
+++ OBSCounter/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 4
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 2
+#define VERSION_BUILD 3
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
