593ce9a999987c3b53ede9e9f86593d5d20bc4af
ubelhj <ubelhj@uw.edu>
2021-08-17 13:53:23 -0700

    Updates version number to 4.0.0.x

--- OBSCounter/version.h
+++ OBSCounter/version.h
@@ -1,8 +1,8 @@
 #pragma once
-#define VERSION_MAJOR 3
+#define VERSION_MAJOR 4
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 84
+#define VERSION_BUILD 2
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
